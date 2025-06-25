93f127a8650ef0e37ed0675c6f2416754d64f4a5
handsomegit <30sectowc@gmail.com>
2025-06-25 19:35:48 +0800

    Update build.sh

--- app/build.sh
+++ app/build.sh
@@ -2,4 +2,4 @@ docker build -t yolov8-rknn-api .
 
 
 
-docker run --rm -it --net=host --privileged   -v /dev:/dev   -p 8000:8000   yolov8-rknn-api
+docker run --rm -it --net=host --privileged  -p 8000:8000   yolov8-rknn-api
