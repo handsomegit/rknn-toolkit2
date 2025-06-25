68869b877f5c5ccd36c456179ba6a8c96697dbc3
handsomegit <30sectowc@gmail.com>
2025-06-25 19:34:21 +0800

    Create yolov8_infer.py

---
+++ app/yolov8_infer.py
@@ -0,0 +1,165 @@
+import numpy as np
+
+OBJ_THRESH = 0.25
+NMS_THRESH = 0.45
+IMG_SIZE = (640, 640)  # width, height
+
+CLASSES = (
+    "person", "bicycle", "car","motorbike ","aeroplane ","bus ","train","truck ","boat","traffic light",
+    "fire hydrant","stop sign ","parking meter","bench","bird","cat","dog ","horse ","sheep","cow","elephant",
+    "bear","zebra ","giraffe","backpack","umbrella","handbag","tie","suitcase","frisbee","skis","snowboard","sports ball","kite",
+    "baseball bat","baseball glove","skateboard","surfboard","tennis racket","bottle","wine glass","cup","fork","knife ",
+    "spoon","bowl","banana","apple","sandwich","orange","broccoli","carrot","hot dog","pizza ","donut","cake","chair","sofa",
+    "pottedplant","bed","diningtable","toilet ","tvmonitor","laptop	","mouse	","remote ","keyboard ","cell phone","microwave ",
+    "oven ","toaster","sink","refrigerator ","book","clock","vase","scissors ","teddy bear ","hair drier", "toothbrush "
+)
+
+def filter_boxes(boxes, box_confidences, box_class_probs):
+    box_confidences = box_confidences.reshape(-1)
+    candidate, class_num = box_class_probs.shape
+
+    class_max_score = np.max(box_class_probs, axis=-1)
+    classes = np.argmax(box_class_probs, axis=-1)
+
+    _class_pos = np.where(class_max_score * box_confidences >= OBJ_THRESH)
+    scores = (class_max_score * box_confidences)[_class_pos]
+
+    boxes = boxes[_class_pos]
+    classes = classes[_class_pos]
+
+    return boxes, classes, scores
+
+def nms_boxes(boxes, scores):
+    x = boxes[:, 0]
+    y = boxes[:, 1]
+    w = boxes[:, 2] - boxes[:, 0]
+    h = boxes[:, 3] - boxes[:, 1]
+
+    areas = w * h
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+
+        xx1 = np.maximum(x[i], x[order[1:]])
+        yy1 = np.maximum(y[i], y[order[1:]])
+        xx2 = np.minimum(x[i] + w[i], x[order[1:]] + w[order[1:]])
+        yy2 = np.minimum(y[i] + h[i], y[order[1:]] + h[order[1:]])
+
+        w1 = np.maximum(0.0, xx2 - xx1 + 0.00001)
+        h1 = np.maximum(0.0, yy2 - yy1 + 0.00001)
+        inter = w1 * h1
+
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        inds = np.where(ovr <= NMS_THRESH)[0]
+        order = order[inds + 1]
+    return np.array(keep)
+
+def dfl(position):
+    import torch
+    x = torch.tensor(position)
+    n, c, h, w = x.shape
+    p_num = 4
+    mc = c // p_num
+    y = x.reshape(n, p_num, mc, h, w)
+    y = y.softmax(2)
+    acc_metrix = torch.arange(mc).float().reshape(1, 1, mc, 1, 1)
+    y = (y * acc_metrix).sum(2)
+    return y.numpy()
+
+def box_process(position):
+    grid_h, grid_w = position.shape[2:4]
+    col, row = np.meshgrid(np.arange(0, grid_w), np.arange(0, grid_h))
+    col = col.reshape(1, 1, grid_h, grid_w)
+    row = row.reshape(1, 1, grid_h, grid_w)
+    grid = np.concatenate((col, row), axis=1)
+    stride = np.array([IMG_SIZE[1] // grid_h, IMG_SIZE[0] // grid_w]).reshape(1, 2, 1, 1)
+
+    position = dfl(position)
+    box_xy = grid + 0.5 - position[:, 0:2, :, :]
+    box_xy2 = grid + 0.5 + position[:, 2:4, :, :]
+    xyxy = np.concatenate((box_xy * stride, box_xy2 * stride), axis=1)
+
+    return xyxy
+
+def post_process(input_data):
+    boxes, scores, classes_conf = [], [], []
+    default_branch = 3
+    pair_per_branch = len(input_data) // default_branch
+    for i in range(default_branch):
+        boxes.append(box_process(input_data[pair_per_branch * i]))
+        classes_conf.append(input_data[pair_per_branch * i + 1])
+        scores.append(np.ones_like(input_data[pair_per_branch * i + 1][:, :1, :, :], dtype=np.float32))
+
+    def sp_flatten(_in):
+        ch = _in.shape[1]
+        _in = _in.transpose(0, 2, 3, 1)
+        return _in.reshape(-1, ch)
+
+    boxes = [sp_flatten(_v) for _v in boxes]
+    classes_conf = [sp_flatten(_v) for _v in classes_conf]
+    scores = [sp_flatten(_v) for _v in scores]
+
+    boxes = np.concatenate(boxes)
+    classes_conf = np.concatenate(classes_conf)
+    scores = np.concatenate(scores)
+
+    boxes, classes, scores = filter_boxes(boxes, scores, classes_conf)
+
+    nboxes, nclasses, nscores = [], [], []
+    for c in set(classes):
+        inds = np.where(classes == c)
+        b = boxes[inds]
+        c = classes[inds]
+        s = scores[inds]
+        keep = nms_boxes(b, s)
+        if len(keep) != 0:
+            nboxes.append(b[keep])
+            nclasses.append(c[keep])
+            nscores.append(s[keep])
+
+    if not nclasses and not nscores:
+        return None, None, None
+
+    boxes = np.concatenate(nboxes)
+    classes = np.concatenate(nclasses)
+    scores = np.concatenate(nscores)
+
+    return boxes, classes, scores
+
+def draw(image, boxes, scores, classes):
+    import cv2
+    for box, score, cl in zip(boxes, scores, classes):
+        top, left, right, bottom = [int(b) for b in box]
+        cv2.rectangle(image, (top, left), (right, bottom), (255, 0, 0), 2)
+        cv2.putText(image, f'{CLASSES[cl]} {score:.2f}', (top, left - 6),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+def setup_model(args):
+    model_path = args.model_path
+    if model_path.endswith('.pt') or model_path.endswith('.torchscript'):
+        platform = 'pytorch'
+        from py_utils.pytorch_executor import Torch_model_container
+        model = Torch_model_container(args.model_path)
+    elif model_path.endswith('.rknn'):
+        platform = 'rknn'
+        from py_utils.rknn_executor import RKNN_model_container
+        model = RKNN_model_container(args.model_path, args.target, args.device_id)
+    elif model_path.endswith('onnx'):
+        platform = 'onnx'
+        from py_utils.onnx_executor import ONNX_model_container
+        model = ONNX_model_container(args.model_path)
+    else:
+        raise RuntimeError(f"{model_path} is not rknn/pytorch/onnx model")
+    print(f'Model-{model_path} is {platform} model, starting val')
+    return model, platform
+
+
+__all__ = [
+    "setup_model",
+    "post_process",
+    "CLASSES",
+    "draw"
+]
